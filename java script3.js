1c2a428fa8af1d574c39829872d73775afac7649
rajkumaraddala <115741752+rajkumaraddala@users.noreply.github.com>
2022-11-07 00:54:07 +0530

    Add files via upload

---
+++ java script3.js
@@ -0,0 +1,58 @@
+console.log("My age is : 100");
+console.log("Days in Coding : 3weeks");
+//opening line
+console.log('It was love at first sight.');
+
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+
+console.log('JavaScript');
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49 );
+
+
+
+
+console.log(100 + 3.5);
+console.log(2022 - 1969);
+console.log(240 / 65);
+console.log(100 * 0.2708);
+
+
+
+console.log("Hello world");
+console.log("Hello"+" "+"World");
+
+
+
+
+
+let name1 = "Teaching the world how to code";
+console.log(name1.length);
+
+
+
+
+let name2 = 'Codecademy';
+console.log(nmae2.toUpperCase());
+
+let name3 = 'Remove whitespace';
+console.log(name3.trim());
+
+
+
+
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+
+console.log(Math.floor(43.8));
+
+
+console.log(Number.isInteger(2017));
+
+
